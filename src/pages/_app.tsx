2b265c033e963b7e0cc9ecf084e639e3dae2fab3
Leo-fse <morimoto.ke@gmail.com>
2022-09-18 20:13:52 +0900

    Mantineのインストールとtailwindとの調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
-
-import "../style.css";
-import "../App.css";
+import "src/lib/tailwind.css";
+import { MantineProvider } from "@mantine/core";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  </>
+  )
 }
